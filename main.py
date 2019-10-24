96958d2117ec7714cfc4704cbd2b0c30a4ca93d7
tiago <tiago.tmleite@gmail.com>
2019-10-24 07:47:18 -0300

    reddit graphs

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import networkx.generators as gen
 import networkx as nx
 import uuid
 import random
+from node2vec import Node2Vec
 
 random.seed(29)
 
@@ -41,14 +42,12 @@ def avg_degree(g):
     size = len(degrees)
     for x in degrees:
         soma = soma + x[1]
-    degrees_mean = soma/size
+    degrees_mean = soma / size
     return degrees_mean
 
 
 def generate_networks():
-
     for k in range(1000):
-
         print(k)
 
         g = gen.watts_strogatz_graph(n=178, k=random.randrange(10, 100), p=.5)
@@ -68,26 +67,75 @@ def generate_networks():
         rcm = list(reverse_cuthill_mckee_ordering(m))
         m_adj = nx.adjacency_matrix(m, nodelist=rcm).toarray()
 
-        plt.imsave('img/0/'+str(k)+'.jpg', g_adj, cmap='gray')
-        plt.imsave('img/1/'+str(k)+'.jpg', h_adj, cmap='gray')
-        plt.imsave('img/2/'+str(k)+'.jpg', j_adj, cmap='gray')
-        plt.imsave('img/3/'+str(k)+'.jpg', m_adj, cmap='gray')
+        plt.imsave('img/0/' + str(k) + '.jpg', g_adj, cmap='gray')
+        plt.imsave('img/1/' + str(k) + '.jpg', h_adj, cmap='gray')
+        plt.imsave('img/2/' + str(k) + '.jpg', j_adj, cmap='gray')
+        plt.imsave('img/3/' + str(k) + '.jpg', m_adj, cmap='gray')
 
         # print(nx.number_of_edges(g), nx.number_of_edges(h), nx.number_of_edges(j))
 
         # print(avg_degree(g), avg_degree(h), avg_degree(j), avg_degree(m))
 
 
-generate_networks()
+def parse_files():
+    indicator = pd.read_csv('graph_data/proteins/PROTEINS_full_graph_indicator.csv')
+    labels = pd.read_csv('graph_data/proteins/PROTEINS_full_graph_labels.csv')
+    graph_ids = list((indicator['graph_id']))
+    graph_class = [labels.iloc[graph_id - 1].values[0] for graph_id in graph_ids]
+    indexes = list(np.arange(1, len(graph_ids) + 1))
+    dataframe = pd.DataFrame(data={'node_id': indexes, 'graph_id': graph_ids, 'class': graph_class})
+    dataframe.to_csv('graph_data/proteins/node_graph_class.csv', index=False)
 
 
+def build_graphs():
+    graph_data = pd.read_csv('graph_data/proteins/node_graph_class.csv')
+    edges = pd.read_csv('graph_data/proteins/PROTEINS_full_A.csv')
+    total_graphs = len(graph_data['graph_id'].unique())
+    print(total_graphs)
+    last_id = -1
+    all_graps = []
+    total = len(edges)
+    for index, row in edges.iterrows():
+        print(index, 'of', total)
+        node1 = int(row['node1'])
+        node2 = int(row['node2'])
+        graph_id = graph_data[graph_data['node_id'] == node1].iloc[0]['graph_id']
+
+        if last_id != graph_id:
+            last_id = graph_id
+            current_graph = nx.Graph()
+            all_graps.append(current_graph)
+
+        if node1 not in current_graph:
+            current_graph.add_node(node1)
+
+        if node2 not in current_graph:
+            current_graph.add_node(node2)
+
+        current_graph.add_edge(node1, node2)
+
+    return all_graps
 
 
+graphs = build_graphs()
 
+print("Total graphs:", len(graphs))
+nodes_avg = [len(g.nodes) for g in graphs]
+print(sorted(nodes_avg))
+nodes_avg = sorted(nodes_avg)
+unique, counts = np.unique(nodes_avg, return_counts=True)
+print(np.asarray((unique, counts)).T)
 
+'''
+for g in graphs:
+    adj = nx.adjacency_matrix(g).toarray()
+    print(adj)
+    print(np.shape(adj))'''
 
-
-
-
-
-
+# generate_networks()
+'''graph = nx.barabasi_albert_graph(n=178, m=14, seed=5)
+node2vec = Node2Vec(graph, dimensions=64, workers=8)
+model = node2vec.fit()
+emb = model.wv.get_vector('12')
+print(np.shape(emb))
+print(emb)'''

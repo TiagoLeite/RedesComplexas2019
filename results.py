3e6a1bf525aea6536e06ba4586438bde2310726b
tiago <tiago.tmleite@gmail.com>
2019-11-01 09:11:18 -0300

    results, imdb, pdfs

--- results.py
+++ results.py
@@ -4,17 +4,17 @@ import numpy as np
 dfs = list()
 max = list()
 for k in range(10):
-    df = pd.read_csv('proteins/log/training'+str(k)+'.log')['val_acc']
+    df = pd.read_csv('imdb/log/training'+str(k)+'.log')['val_acc']
     dfs.append(df)
     max.append(np.max(df))
 
 print(max)
 print(np.mean(max), '+-', np.std(max))
 
-df = pd.read_csv('proteins/log/training' + str(0) + '.log')['val_loss']
+df = pd.read_csv('imdb/log/training' + str(0) + '.log')['val_loss']
 
-for line in df:
-    print(line)
+#for line in df:
+#    print(line)
 
 
 
